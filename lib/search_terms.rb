4fadf3847fea106de7525ae15d0254c2424619f6
beepscore <support@beepscore.com>
2012-04-13 00:14:46 -0700

    In search_terms.rb initialize, read file and set :file_string.
    In search_terms_test.rb add method test_file_string.
    All unit tests pass.

--- lib/search_terms.rb
+++ lib/search_terms.rb
@@ -4,9 +4,20 @@
 class SearchTerms
 
   attr_reader :filename
+  attr_reader :file_string
 
   def initialize(filename)
     @filename = filename
+
+    search_terms_file = File.open('data/my_file.txt', 'r')
+
+    @file_string = ""
+    search_terms_file.each_line do |line|
+      puts "line : #{line}"
+      @file_string += "#{line}"
+    end
+
+    search_terms_file.close
   end
 
 end
--- test/search_terms_test.rb
+++ test/search_terms_test.rb
@@ -20,4 +20,11 @@ class SearchTermsTest < MiniTest::Unit::TestCase
     assert_equal(expected_result, actual_result)
   end
 
+  def test_file_string()
+    a_search_terms = SearchTerms.new('data/my_file.txt')
+    actual_result = a_search_terms.file_string
+
+    expected_result = "Monday\n"
+    assert_equal(expected_result, actual_result)
+  end
 end
