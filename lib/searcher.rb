860839203ffbe923708b66071f5b63a571985a99
beepscore <support@beepscore.com>
2012-04-14 20:18:43 -0700

    In searcher.rb add attribute search_terms. Add initialize(search_terms).
    In searcher_test.rb add test_searcher_search_terms.

--- lib/searcher.rb
+++ lib/searcher.rb
@@ -3,6 +3,13 @@
 
 class Searcher
 
+  attr_reader :search_terms
+
+  def initialize(search_terms)
+    @search_terms = search_terms
+  end
+
+
   # Current implementation of searcher doesn't use its parameters,
   # returns ls of current directory
   # TODO: Use parameters and ack to search for files containing search term
--- test/searcher_test.rb
+++ test/searcher_test.rb
@@ -11,8 +11,17 @@ class SearcherTest < MiniTest::Unit::TestCase
   def teardown()
   end
 
+
+  def test_searcher_search_terms()
+    a_searcher = Searcher.new(['boo', 'far'])
+    actual_result = a_searcher.search_terms
+    expected_result = ['boo', 'far']
+    assert_equal(expected_result, actual_result)
+  end
+
+
   def test_search()
-    a_searcher = Searcher.new()
+    a_searcher = Searcher.new(['squirrel', 'chipmunk'])
     actual_result = a_searcher.search('directory_dont_care', 'search_term_dont_care')
     expected_result = "README.md\nRakefile\ndata\nlib\ntest\n"
     assert_equal(expected_result, actual_result)
