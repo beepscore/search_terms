0bb6ce09ebf3d918b316e86fd9dd1e230eb7552c
beepscore <support@beepscore.com>
2012-04-14 17:46:39 -0700

    In test_file_encoding test Localizable.strings

--- test/search_terms_test.rb
+++ test/search_terms_test.rb
@@ -14,7 +14,16 @@ class SearchTermsTest < MiniTest::Unit::TestCase
   def test_file_encoding()
     a_search_terms = SearchTerms.new('data/my_file.txt')
     actual_result = a_search_terms.file_encoding
+    expected_result = Encoding.find('UTF-8')
+    assert_equal(expected_result, actual_result)
 
+    a_search_terms = SearchTerms.new('data/Localizable.strings')
+    actual_result = a_search_terms.file_encoding
+    # Terminal shows system considers Localizable.strings as UTF-16LE 
+    # data$ file Localizable.strings
+    # Localizable.strings: Little-endian UTF-16 Unicode c program text
+    # expected_result = Encoding.find('UTF-16LE')
+    # Currently ruby assumes file is UTF-8
     expected_result = Encoding.find('UTF-8')
     assert_equal(expected_result, actual_result)
   end
