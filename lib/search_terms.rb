59f78cdfd0f82eb1e4a6d359f107ad759f3223ed
beepscore <support@beepscore.com>
2012-04-14 23:51:24 -0700

    In search_terms.rb add attribute search_terms_array and set it in configure_search_terms.

--- lib/search_terms.rb
+++ lib/search_terms.rb
@@ -7,6 +7,7 @@ class SearchTerms
   attr_reader :file_name
   attr_reader :file_string
   attr_reader :search_terms_string
+  attr_reader :search_terms_array
 
   def initialize(file_name, external_encoding)
     @file_name = file_name
@@ -38,12 +39,15 @@ class SearchTerms
   def configure_search_terms
 
     @search_terms_string = ""
+    @search_terms_array = []
     @file_string.each_line do |line|
       if ("\"" == line[0])
         # line starts with ", assume it is a valid search term with a second ".
         # split line at " characters, take element 1, re-delimit with ""
         # add search term       
-        @search_terms_string += "\"" + line.split("\"")[1] + "\"\n"
+        search_term = "\"" + line.split("\"")[1] + "\""
+        @search_terms_string += search_term + "\n"
+        @search_terms_array.push(search_term)
       end
     end
     puts @search_terms_string
